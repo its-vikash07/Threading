9f729a8d7e27c8d661d9eb45260c4e18fd8e33c1
VIKASH MANDLOI <126850164+its-vikash07@users.noreply.github.com>
2024-05-02 10:10:07 +0530

    Add files via upload

---
+++ InterruptException.java
@@ -0,0 +1,22 @@
+package Theading;
+
+public class InterruptException extends Thread {
+    public void run(){
+        try {
+            for(int i=1;i<5;i++){
+                System.out.println(i);
+                Thread.sleep(1000);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        InterruptException t=new InterruptException();
+
+        t.start();
+        t.interrupt();
+    }
+    
+}
